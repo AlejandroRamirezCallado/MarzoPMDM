cceabc11d759e806a54fb89b550afe25ebf6fec1
Alejandro <alejandro@MacBook-Pro-de-Alejandro.local>
2018-02-27 14:39:28 +0100

    SecondActivityEvents

--- android/src/com/marzopmdm/SecondActivity.java
+++ android/src/com/marzopmdm/SecondActivity.java
@@ -4,14 +4,42 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class   SecondActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+
+public class SecondActivity extends AppCompatActivity {
+   // ListaFragment listaFragmentMensajes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        SecondActivityEvents events = new SecondActivityEvents();
+        DataHolder.instance.fireBaseAdmin.setListener(events);
 
-        //SecondActivityEvents events = new SecondActivityEvents(this);
-        //DataHolder.instance.fireBaseAdmin.setListener(events);
+    }
+
+}
+class class SecondActivityEvents implements FireBaseAdminListener{
+    SecondActivity secondActivity;
+
+    public SecondActivityEvents(SecondActivity secondActivity){
+        this.secondActivity=secondActivity;
+    }
+
+
+
+    @Override
+    public void fireBaseAdmin_RegisterOK(boolean blOK) {
+
+    }
+
+    @Override
+    public void fireBaseAdmin_LoginOK(boolean blOK) {
+
+    }
+
+    @Override
+    public void fireBaseAdmin_RamaDescargargada(String rama, DataSnapshot dataSnapshot) {
+
     }
 }
